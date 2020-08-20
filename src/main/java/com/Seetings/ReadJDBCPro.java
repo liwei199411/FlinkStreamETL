b56685fb60c697876de2f3604538447d5e5b1670
liwei199411 <liwei_199411@126.com>
2020-08-20 21:15:47 +0800

    Update ReadJDBCPro.java

--- src/main/java/com/Seetings/ReadJDBCPro.java
+++ src/main/java/com/Seetings/ReadJDBCPro.java
@@ -12,16 +12,16 @@ public class ReadJDBCPro {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        properties.setProperty("url","jdbc:postgresql://10.252.97.201:5432/datahub?serverTimezone=GMT+8");
-        properties.setProperty("Username","ur_uapp_data");
-        properties.setProperty("Password","D@TAtstuapp201");
+        properties.setProperty("url","jdbc:postgresql://******:5432/datahub?serverTimezone=GMT+8");
+        properties.setProperty("Username","******");
+        properties.setProperty("Password","******");
         return properties;
     }
     public static Properties buildMysqlJDBCProps(){
         Properties properties = new Properties();
-        properties.setProperty("url","jdbc:mysql://master:3306/canal_destination??useUnicode=true&characterEncoding=UTF-8");
-        properties.setProperty("Username","root");
-        properties.setProperty("Password","root");
+        properties.setProperty("url","jdbc:mysql://******:3306/canal_destination??useUnicode=true&characterEncoding=UTF-8");
+        properties.setProperty("Username","*****");
+        properties.setProperty("Password","*****");
         return properties;
     }
 
